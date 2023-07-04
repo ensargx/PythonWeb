bdb117f8f6890ce15a86bb4b03399b90b8fbdeb9
Ensar Gök <ensarxgok@gmail.com>
2023-07-04 19:17:07 +0300

    CORS

--- fastapi_jwt/main.py
+++ fastapi_jwt/main.py
@@ -6,6 +6,16 @@ from todo import todo_router
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 # /auth router
 app.include_router(auth_router, prefix="/auth")
 
