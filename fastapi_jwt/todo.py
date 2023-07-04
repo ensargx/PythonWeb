368f77be8c56932880a8848573d33501e5e9ac03
Ensar Gök <ensarxgok@gmail.com>
2023-07-04 18:21:16 +0300

    little change

--- fastapi_jwt/todo.py
+++ fastapi_jwt/todo.py
@@ -49,7 +49,7 @@ async def shutdown():
 async def get_todo_by_id(todo_id):
     pass
 
-@todo_router.get("s/", response_model=list[Todo])
+@todo_router.get("/", response_model=list[Todo])
 async def get_todos():
     query = todos.select()
     return await database.fetch_all(query)
