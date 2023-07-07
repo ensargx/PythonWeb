2e69028cbec1e70b3b2a9aa1330ef41a75fd2fb1
Ensar Gök <ensarxgok@gmail.com>
2023-07-07 11:29:32 +0300

    frontend update for auth

--- fastapi_jwt/frontend/script/main.js
+++ fastapi_jwt/frontend/script/main.js
@@ -10,7 +10,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 // create function to fetch todos from backend and return as json
 const getTodos = () => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield fetch("http://localhost:8000/todo/");
+    // if token is not present, alert user that they are not logged in
+    if (!check_token()) {
+        return;
+    }
+    const response = yield fetch("http://localhost:8000/todo/", {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+    });
     const todos = yield response.json();
     return todos;
 });
@@ -44,6 +52,9 @@ const renderTodos = () => __awaiter(void 0, void 0, void 0, function* () {
 const deleteTodo = (id) => __awaiter(void 0, void 0, void 0, function* () {
     const response = yield fetch(`http://localhost:8000/todo/${id}`, {
         method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
     });
     const data = yield response.json();
     console.log(data);
@@ -72,6 +83,7 @@ const addTodo = () => __awaiter(void 0, void 0, void 0, function* () {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
         },
         body: JSON.stringify({ title, description, owner_id: 1 }),
     });
@@ -94,6 +106,7 @@ const addTodo = () => __awaiter(void 0, void 0, void 0, function* () {
     todoDiv.innerHTML = `
         <h3>${todo.title}</h3>
         <p>${todo.description}</p>
+        <input type="checkbox" onclick="completeTodo(${todo.id}); return false;" ${todo.completed}>Completed</checkbox>
         <button class="delete" onclick="deleteTodo(${todo.id}); return false;">Delete</button>
     `;
     todosDiv.appendChild(todoDiv);
@@ -122,6 +135,7 @@ const updateTodo = (id, values) => __awaiter(void 0, void 0, void 0, function* (
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
         },
         body: JSON.stringify(values),
     });
@@ -132,7 +146,114 @@ const updateTodo = (id, values) => __awaiter(void 0, void 0, void 0, function* (
     }
     return 1;
 });
+const check_token = () => __awaiter(void 0, void 0, void 0, function* () {
+    // get token from local storage
+    const token = localStorage.getItem("access_token");
+    if (!token) {
+        return false;
+    }
+    return true;
+});
+const login = () => __awaiter(void 0, void 0, void 0, function* () {
+    // get username and password from input fields
+    const form = document.getElementById("login_form");
+    const username = form.username.value;
+    const password = form.password.value;
+    // clear input fields
+    form.username.value = "";
+    form.password.value = "";
+    // send username and password to backend
+    const response = yield fetch("http://localhost:8000/user/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+    });
+    const data = yield response.json();
+    // if response_status_code is 400, alert user that todo was not added
+    if (!response.ok) {
+        alert("Failed to login.\nDetail: " + data.detail);
+        console.log(data);
+        return;
+    }
+    // get token from response
+    const token = data;
+    // save token to local storage
+    localStorage.setItem("access_token", token.access_token);
+    localStorage.setItem("token_type", token.token_type);
+    // delete login form
+    const loginForm = document.getElementById("login_form");
+    loginForm.remove();
+    // fetch todos from backend
+    const todos = yield renderTodos();
+    // create logout button
+    const logoutButton = document.createElement("button");
+    logoutButton.id = "logout_button";
+    logoutButton.innerHTML = "Logout";
+    logoutButton.onclick = logout;
+    document.body.appendChild(logoutButton);
+});
+const create_login_form = () => {
+    // if exists, delete logout button
+    const logoutButton = document.getElementById("logout_button");
+    if (logoutButton)
+        logoutButton.remove();
+    // create login form into div with id "login_form"
+    const loginForm = document.createElement("form");
+    loginForm.id = "login_form";
+    loginForm.innerHTML = `
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" required>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" required> 
+        <button type="submit">Login</button>
+    `;
+    loginForm.onsubmit = (e) => {
+        e.preventDefault();
+        login();
+    };
+    document.body.appendChild(loginForm);
+};
+const logout = () => __awaiter(void 0, void 0, void 0, function* () {
+    // delete token from local storage
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("token_type");
+    // delete logout button
+    const logoutButton = document.getElementById("logout_button");
+    if (!logoutButton)
+        return;
+    logoutButton.remove();
+    create_login_form();
+    // remove todos from page
+    const todosDiv = document.getElementById("todo_list");
+    if (!todosDiv)
+        return;
+    todosDiv.innerHTML = "";
+});
+const create_logout_button = () => {
+    // if exists, delete login form
+    const loginForm = document.getElementById("login_form");
+    if (loginForm)
+        loginForm.remove();
+    // create logout button
+    const logoutButton = document.createElement("button");
+    logoutButton.id = "logout_button";
+    logoutButton.innerHTML = "Logout";
+    logoutButton.onclick = logout;
+    document.body.appendChild(logoutButton);
+};
 window.onload = () => {
     console.log("loaded");
     renderTodos();
+    // check if token exists
+    const token = localStorage.getItem("access_token");
+    if (!token) {
+        console.log("no token");
+        create_login_form();
+    }
+    else {
+        console.log("token exists");
+        create_logout_button();
+    }
 };
--- fastapi_jwt/frontend/src/main.ts
+++ fastapi_jwt/frontend/src/main.ts
@@ -10,7 +10,15 @@ type Todo = {
 
 // create function to fetch todos from backend and return as json
 const getTodos = async () => {
-    const response = await fetch("http://localhost:8000/todo/");
+    // if token is not present, alert user that they are not logged in
+    if(!check_token()) {
+        return;
+    }
+    const response = await fetch("http://localhost:8000/todo/", {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
+    });
     const todos = await response.json();
     return todos;
 };
@@ -47,6 +55,9 @@ const renderTodos = async () => {
 const deleteTodo = async (id: number) => {
     const response = await fetch(`http://localhost:8000/todo/${id}`, {
         method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+        },
     });
     const data = await response.json();
     console.log(data);
@@ -78,6 +89,7 @@ const addTodo = async () => {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
         },
         body: JSON.stringify({ title, description, owner_id: 1 }),
     });
@@ -102,6 +114,7 @@ const addTodo = async () => {
     todoDiv.innerHTML = `
         <h3>${todo.title}</h3>
         <p>${todo.description}</p>
+        <input type="checkbox" onclick="completeTodo(${todo.id}); return false;" ${todo.completed}>Completed</checkbox>
         <button class="delete" onclick="deleteTodo(${todo.id}); return false;">Delete</button>
     `;
     todosDiv.appendChild(todoDiv);
@@ -137,6 +150,7 @@ const updateTodo = async (id: number, values: Todo | any) => {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
         },
         body: JSON.stringify(values),
     });
@@ -148,7 +162,135 @@ const updateTodo = async (id: number, values: Todo | any) => {
     return 1;
 };
 
+const check_token = async () => {
+    // get token from local storage
+    const token = localStorage.getItem("access_token");
+    if(!token) {
+        return false
+    }
+    return true;
+}
+
+type UserToken = {
+    access_token: string;
+    token_type: string;
+};
+
+const login = async () => {
+    // get username and password from input fields
+    const form = document.getElementById("login_form") as HTMLFormElement;
+    const username = form.username.value;
+    const password = form.password.value;
+
+    // clear input fields
+    form.username.value = "";
+    form.password.value = "";
+
+    // send username and password to backend
+    const response = await fetch("http://localhost:8000/user/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+    });
+    const data = await response.json();
+
+    // if response_status_code is 400, alert user that todo was not added
+    if(!response.ok) {
+        alert("Failed to login.\nDetail: " + data.detail);
+        console.log(data);
+        return;
+    }
+
+    // get token from response
+    const token = data as UserToken;
+
+    // save token to local storage
+    localStorage.setItem("access_token", token.access_token);
+    localStorage.setItem("token_type", token.token_type);
+
+    // delete login form
+    const loginForm = document.getElementById("login_form") as HTMLFormElement;
+    loginForm.remove();
+
+    // fetch todos from backend
+    const todos = await renderTodos();
+    
+
+    // create logout button
+    const logoutButton = document.createElement("button");
+    logoutButton.id = "logout_button";
+    logoutButton.innerHTML = "Logout";
+    logoutButton.onclick = logout;
+    document.body.appendChild(logoutButton);
+
+}
+
+const create_login_form = () => {
+    // if exists, delete logout button
+    const logoutButton = document.getElementById("logout_button");
+    if(logoutButton) logoutButton.remove();
+    
+    // create login form into div with id "login_form"
+    const loginForm = document.createElement("form");
+    loginForm.id = "login_form";
+    loginForm.innerHTML = `
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" required>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" required> 
+        <button type="submit">Login</button>
+    `;
+    loginForm.onsubmit = (e) => {
+        e.preventDefault();
+        login();
+    };
+    document.body.appendChild(loginForm);
+}
+
+
+const logout = async () => {
+    // delete token from local storage
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("token_type");
+
+    // delete logout button
+    const logoutButton = document.getElementById("logout_button");
+    if(!logoutButton) return;
+    logoutButton.remove();
+    create_login_form();
+
+    // remove todos from page
+    const todosDiv = document.getElementById("todo_list");
+    if(!todosDiv) return;
+    todosDiv.innerHTML = "";
+}
+
+const create_logout_button = () => {
+    // if exists, delete login form
+    const loginForm = document.getElementById("login_form");
+    if(loginForm) loginForm.remove();
+
+    // create logout button
+    const logoutButton = document.createElement("button");
+    logoutButton.id = "logout_button";
+    logoutButton.innerHTML = "Logout";
+    logoutButton.onclick = logout;
+    document.body.appendChild(logoutButton);
+}
+
 window.onload = () => {
     console.log("loaded");
     renderTodos();
+    // check if token exists
+    const token = localStorage.getItem("access_token");
+    if(!token) {
+        console.log("no token");
+        create_login_form();
+    }
+    else {
+        console.log("token exists");
+        create_logout_button();
+    }
 }
