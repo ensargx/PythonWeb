72f3a93ba894f0fcf610bca5d969c5a2a6387151
Ensar Gök <ensarxgok@gmail.com>
2023-07-09 16:54:45 +0300

    restart node

--- fastapi_jwt/main.py
+++ fastapi_jwt/main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, WebSocket
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from user import user_router, JWTBearer, TokenData, UserLogin, UserIn
 from user import login as user_login
 from user import register as user_register
-from todo import todo_router
+from todo import todo_router, get_todos
 
 app = FastAPI(docs_url = "/api/docs")
 
@@ -29,7 +29,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-# app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -50,16 +50,47 @@ def is_logged_in(request: Request):
 
 @app.get("/")
 async def home(request: Request):
-
     user = is_logged_in(request)
     # render template from templates/index.html
-    return templates.TemplateResponse("index.html", {"request": request, "user": user})
+    flush = request.query_params.get("status")
+    data = {}
+    if flush:
+        print(flush)
+        flush = flush.lower()
+        if flush == "logged_in":
+            data["status"] = "success"
+            data["message"] = "You are logged in"
+        elif flush == "logged_out":
+            print("logged out")
+            data["status"] = "success"
+            data["message"] = "You are logged out"
+        elif flush == "allready_logged_in":
+            data["status"] = "error"
+            data["message"] = "You are allready logged in"
+        else:
+            flush = None
+    if not user:
+        return templates.TemplateResponse("index.html", {"request": request, "data": data})
+    
+    todos = await get_todos(user.dict())
+    return templates.TemplateResponse("home.html", {"request": request, "user": user, "data": data, "todos": todos})
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    print("websocket accepted")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+    except:
+        print("websocket closed")
 
 @app.get("/login")
 async def login(request: Request):
 
     if is_logged_in(request):
-        return RedirectResponse(url="/", status_code=302)    
+        return RedirectResponse(url="/?status=allready_logged_in", status_code=302)
 
     return templates.TemplateResponse("login.html", {"request": request})
 
@@ -85,21 +116,21 @@ async def login(request: Request):
         return templates.TemplateResponse("login.html", {"request": request, "error": response['detail']})
 
     headers = {"Set-Cookie": f"access_token={response['access_token']}; HttpOnly"}
-    response = RedirectResponse(url="/", status_code=302, headers=headers)
+    response = RedirectResponse(url="/?status=logged_in", status_code=302, headers=headers)
     return response
 
 @app.get("/logout")
 async def logout(request: Request):
     
     headers = {"Set-Cookie": "access_token=; HttpOnly"}
-    response = RedirectResponse(url="/", status_code=302, headers=headers)
+    response = RedirectResponse(url="/?status=logged_out", status_code=302, headers=headers)
     return response
 
 @app.get("/register")
 async def register(request: Request):
 
     if is_logged_in(request):
-        return RedirectResponse(url="/", status_code=302)
+        return RedirectResponse(url="/?status=allready_logged_in", status_code=302)
 
     return templates.TemplateResponse("register.html", {"request": request})
 
@@ -117,7 +148,7 @@ async def register(request: Request):
     password = form.get("password")
 
     if not username or not name or not password:
-        return templates.TemplateResponse("register.html", {"request": request, "error": "Username or name or password is empty"})
+        return templates.TemplateResponse("register.html", {"request": request, "data": {"status": "error", "message": "Username, name or password cannot be empty"}})
     
     user_data = UserIn(username=username, name=name, password=password)
     try:
